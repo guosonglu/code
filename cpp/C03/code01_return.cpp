96194ba6c3c3f10910ed0ac0822d100e24feb6de
luguosong <1054595718@qq.com>
2021-05-11 20:32:20 +0800

    2021.5.11

--- cpp/C03/code01_return.cpp
+++ cpp/C03/code01_return.cpp
@@ -6,9 +6,18 @@ char cfunc(int i){
     if(i ==0){
         return 'a';
     }
-    if(i ==)
+    if(i == 1){
+        return 'g';
+    }
+    if(i == 5){
+        return 'z';
+    }
+    return 'c';
 }
 
 int main(){
-    
+   cout << "请输入一个数：";
+   int val;
+   cin >> val;
+   cout << cfunc(val) << endl;
 }
